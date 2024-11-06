a746df3fdd96824e164ba1f0596cfc8d85dbc99b
hanyu <nozidoali@gmail.com>
2024-11-05 16:56:05 -0800

    update: typo in cmakefile

--- tools/prepare_dicke.cpp
+++ tools/prepare_dicke.cpp
@@ -10,7 +10,7 @@ int main()
   QState state_exp = dicke_state( 3, 1 );
   QCircuit qc = prepare_state( state_exp );
   qc = decompose_circuit( qc );
-  QState state_act = simulate( qc, ground_state( 4 ) );
+  QState state_act = simulate( qc, ground_state( 3 ) );
   std::cout << state_act << std::endl;
   std::cout << state_exp << std::endl;
   // write_qasm2( qc, "temp.qasm" );
